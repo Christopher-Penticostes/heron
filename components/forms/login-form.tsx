3b65241f6309ff69b9ea34ab8187271263fc421c
Christopher-Penticostes <168892740+Christopher-Penticostes@users.noreply.github.com>
2026-04-05 20:53:39 +0800

    feat: initial commit

--- components/forms/login-form.tsx
+++ components/forms/login-form.tsx
@@ -13,6 +13,8 @@ import {
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { authClient } from '@/lib/auth-client';
+import Google from '@/public/images/search.png';
+import LoginPic from '@/public/images/Login-pic.png';
 
 export function LoginForm({
   className,
@@ -34,7 +36,7 @@ export function LoginForm({
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
                 <p className="text-balance text-muted-foreground">
-                  Login to your Acme Inc account
+                  Login to your PesoFi account
                 </p>
               </div>
               <Field>
@@ -70,12 +72,7 @@ export function LoginForm({
                   type="button"
                   onClick={handleLoginWithGoogle}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <Image src={Google} alt="Image" width={19} height={19} />
                   Login with Google
                 </Button>
               </Field>
@@ -85,10 +82,10 @@ export function LoginForm({
             </FieldGroup>
           </form>
           <div className="relative hidden bg-muted md:block">
-            <img
-              src="/placeholder.svg"
+            <Image
+              src={LoginPic}
               alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              className="absolute inset-0 h-full w-full object-cover"
             />
           </div>
         </CardContent>
