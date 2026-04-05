dd300c27d635e244f398c12d8c91089510c28395
Christopher-Penticostes <168892740+Christopher-Penticostes@users.noreply.github.com>
2026-04-05 19:45:17 +0800

    feat: initial commit

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,30 @@
+'use client';
+
 import { LoginForm } from '@/components/forms/login-form';
+import Galaxy from '@/components/gradient-bg';
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+    <div className="relative min-h-screen w-full overflow-hidden flex flex-col items-center justify-center p-6 md:p-10">
+      {/* FULLSCREEN BACKGROUND */}
+      <div className="fixed inset-0 w-screen h-screen -z-10 bg-black">
+        <Galaxy
+          mouseRepulsion={false}
+          mouseInteraction={false}
+          density={0.5}
+          glowIntensity={0.1}
+          saturation={0}
+          hueShift={140}
+          twinkleIntensity={0.2}
+          rotationSpeed={0}
+          repulsionStrength={2}
+          autoCenterRepulsion={0}
+          starSpeed={0.1}
+          speed={0.7}
+        />
+      </div>
+
+      {/* CONTENT */}
       <div className="w-full max-w-sm md:max-w-4xl">
         <LoginForm />
       </div>
